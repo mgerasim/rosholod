0df1d15b8a24dc1d82bf279e44f04b026f6b7574
Mikhail Gerasimov <mgerasim@inbox.ru>
2011-11-26 18:17:59 +1000

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rosholod::Application.config.secret_token = '7cc94f511c895e24d764d7f17d0818037163ccb63b7009c77dada7a37a945b026eb25564b6daa200bb43587c537f11685b86523e3e6ea6c5ecdd559fcb52552e'
